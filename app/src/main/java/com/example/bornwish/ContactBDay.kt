740c99355c127884461c455eadeeb34babb65e1b
mohanbalaji95 <mohanbalaji95@yahoo.com>
2019-07-24 07:55:31 -0700

    Initial commit

---
+++ app/src/main/java/com/example/bornwish/ContactBDay.kt
@@ -0,0 +1,121 @@
+package com.example.bornwish
+
+import android.R
+import android.annotation.TargetApi
+import android.app.Notification
+import android.app.Notification.PRIORITY_HIGH
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.os.IBinder
+import android.provider.ContactsContract
+import android.support.annotation.RequiresApi
+import android.support.v4.app.NotificationCompat
+import android.util.Log
+
+
+class ContactBDay : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent?.action.equals("StartForeground")) {
+            startForegroundService()
+            val r = Runnable {
+                val mcontentResolver = contentResolver
+                val projection = arrayOf(ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME)
+                val cursor = mcontentResolver.query(ContactsContract.Contacts.CONTENT_URI, projection, null, null, null)
+                if (cursor!!.count > 0) {
+                    while (cursor.moveToNext()) {
+                        val contactInfoMap = HashMap<String, String>()
+                        val contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Data._ID))
+                        val contactDisplayName =
+                            cursor.getString(cursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME))
+
+                        val columns = arrayOf(
+                            ContactsContract.CommonDataKinds.Event.START_DATE,
+                            ContactsContract.CommonDataKinds.Event.TYPE,
+                            ContactsContract.CommonDataKinds.Event.MIMETYPE
+                        )
+
+                        val where =
+                            ContactsContract.CommonDataKinds.Event.TYPE + "=" + ContactsContract.CommonDataKinds.Event.TYPE_BIRTHDAY +
+                                    " and " + ContactsContract.CommonDataKinds.Event.MIMETYPE + " = '" + ContactsContract.CommonDataKinds.Event.CONTENT_ITEM_TYPE + "' and " + ContactsContract.Data.CONTACT_ID + " = " + contactId
+
+                        val selectionArgs: Array<String>? = null
+                        val sortOrder = ContactsContract.Contacts.DISPLAY_NAME
+
+                        val birthdayCur = mcontentResolver.query(
+                            ContactsContract.Data.CONTENT_URI,
+                            columns,
+                            where,
+                            selectionArgs,
+                            sortOrder
+                        )
+                        if (birthdayCur!!.getCount() > 0) {
+                            while (birthdayCur.moveToNext()) {
+                                val birthday =
+                                    birthdayCur.getString(birthdayCur.getColumnIndex(ContactsContract.CommonDataKinds.Event.START_DATE))
+                                Log.e(
+                                    "Main disp with bday ",
+                                    contactDisplayName + "'s bday is " + birthday + " Contact's Id " + contactId
+                                );
+                            }
+                        }
+                        birthdayCur.close()
+                    }
+                }
+                cursor.close()
+                stopForeground(true)
+                stopSelf()
+            }
+            val queryThread = Thread(r)
+            queryThread.start()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    fun startForegroundService() {
+        Log.d("ContactBDay", "Start foreground service.")
+
+        val channelId =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel("my_service", "My Background Service")
+            } else {
+                ""
+            }
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+        val notification = notificationBuilder.setOngoing(true)
+            .setSmallIcon(R.mipmap.sym_def_app_icon)
+            .setContentText("App is running")
+            .setPriority(PRIORITY_HIGH)
+            .setOngoing(true)
+            .build()
+
+        startForeground(1, notification)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        val chan = NotificationChannel(
+            channelId,
+            channelName, NotificationManager.IMPORTANCE_NONE
+        )
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
+    }
+}
---
+++ app/src/main/java/com/example/bornwish/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.example.bornwish
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_CONTACTS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_CONTACTS), 112
+            );
+        } else {
+            serviceInvocation()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            112 -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    serviceInvocation()
+                } else {
+                    Toast.makeText(this, "The app was not allowed to read your contact", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    fun serviceInvocation() {
+        val intent = Intent(this, ContactBDay::class.java)
+        intent.setAction("StartForeground")
+        startService(intent)
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_home -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_tools -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
